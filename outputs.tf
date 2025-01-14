c0b14d6fe831ddfa5ed3dcf521d01f1d7af02362
gkermo <>
2025-01-14 18:52:29 +0100

    Added Terraform output file

---
+++ outputs.tf
@@ -0,0 +1,7 @@
+output "ecostream_manager_container_id" {
+    value = docker_container.ecostream_manager_container.id
+}
+
+output "ecostream_visualizer_container_id" {
+    value = docker_container.ecostream_visualizer_container.id
+}
